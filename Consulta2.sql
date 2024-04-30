bac9e2776b786f2f3e54352d26213f179f6e662c
SergioLs11 <140862209+SergioLs11@users.noreply.github.com>
2024-04-30 13:11:38 -0500

    Update Consulta2.sql

--- Consulta2.sql
+++ Consulta2.sql
@@ -1,1 +1,8 @@
-
+SELECT mp.cargo AS clase_de_miembro,
+       mp.sexo,
+       SUM(r.monto_reclamado - r.monto_debitado) AS reclamaciones_no_pagadas
+FROM reclamo r
+JOIN cita c ON r.cita_id = c.id
+JOIN miembro_personal mp ON c.miembro_personal_cedula = mp.cedula
+WHERE r.estado <> 'Pagado Totalmente'
+GROUP BY mp.cargo, mp.sexo;
