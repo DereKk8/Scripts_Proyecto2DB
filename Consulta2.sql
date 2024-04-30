61f02ae029ad3c7a3a062238b4afc1f9d770c61a
SergioLs11 <140862209+SergioLs11@users.noreply.github.com>
2024-04-30 13:19:32 -0500

    Update Consulta2.sql

--- Consulta2.sql
+++ Consulta2.sql
@@ -1,8 +1,10 @@
-SELECT mp.cargo AS clase_de_miembro,
-       SUM(CASE WHEN mp.sexo = 'M' THEN (r.monto_reclamado - r.monto_debitado) ELSE 0 END) AS reclamaciones_no_pagadas_masculino,
-       SUM(CASE WHEN mp.sexo = 'F' THEN (r.monto_reclamado - r.monto_debitado) ELSE 0 END) AS reclamaciones_no_pagadas_femenino
-FROM reclamo r
-JOIN cita c ON r.cita_id = c.id
-JOIN miembro_personal mp ON c.miembro_personal_cedula = mp.cedula
-WHERE r.estado <> 'Pagado Totalmente'
-GROUP BY mp.cargo;
+SELECT MP.CARGO AS CLASE_DE_MIEMBRO,
+SUM(CASE WHEN MP.SEXO = 'M' THEN (R.MONTO_RECLAMADO - R.MONTO_DEBITADO) ELSE 0 END) AS RECLAMACIONES_NO_PAGADAS_MASCULINO,
+SUM(CASE WHEN MP.SEXO = 'F' THEN (R.MONTO_RECLAMADO - R.MONTO_DEBITADO) ELSE 0 END) AS RECLAMACIONES_NO_PAGADAS_FEMENINO
+FROM RECLAMO R
+JOIN CITA C 
+ON R.CITA_ID = C.ID
+JOIN MIEMBRO_PERSONAL MP 
+ON C.MIEMBRO_PERSONAL_CEDULA = MP.CEDULA
+WHERE R.ESTADO <> 'PAGO'
+GROUP BY MP.CARGO;
