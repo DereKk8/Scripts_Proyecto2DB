204bb16b6aa01da5cf99ca53a1ff959183ec6996
ErickSalazar07 <150879552+ErickSalazar07@users.noreply.github.com>
2024-04-29 21:44:28 -0500

    Update Consulta3.sql

--- Consulta3.sql
+++ Consulta3.sql
@@ -1,3 +1,3 @@
-SELECT paciente.nombre, representante.nombre, tipo_relacion
-FROM paciente, cita
+SELECT paciente.nombre as Nombre_Paciente, representante.nombre as Nombre_Representante, tipo_relacion
+FROM paciente, representante
 WHERE paciente.representante_cedula = representante.cedula OR paciente.representante_cedula = NULL    
