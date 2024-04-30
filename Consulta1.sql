9080fda20762efa66fba421f8c2e802cd79b4ed0
SergioLs11 <140862209+SergioLs11@users.noreply.github.com>
2024-04-30 13:21:43 -0500

    Update Consulta1.sql

--- Consulta1.sql
+++ Consulta1.sql
@@ -1,11 +1,10 @@
 SELECT AA3.NOMBRE, AA3.SEXO, AA3.NUM_CITA, AA3.MONTO_RECLAMADO, AA3.MONTO_DEBITADO,(AA3.MONTO_RECLAMADO - AA3.MONTO_DEBITADO) AS DIFERENCIA
 FROM
 (
-    -- Subconsulta para obtener los datos originales
     SELECT A2.NOMBRE, A2.SEXO, A2.ID, A2.NUM_CITA, C3.MONTO_RECLAMADO, C4.MONTO_DEBITADO
     FROM 
     (
-        -- Subconsulta para obtener los datos base
+        
         SELECT B1.NOMBRE, B1.SEXO, B1.ID, B2.NUM_CITA
         FROM
         (
@@ -32,7 +31,7 @@ FROM
     INNER JOIN RECLAMO ON A2.ID = RECLAMO.CITA_ID
     INNER JOIN
     (
-        -- Subconsulta para obtener la suma de MONTO_RECLAMADO
+        
         SELECT 'Total' AS NOMBRE, 'Total' AS SEXO, SUM(RECLAMO.MONTO_RECLAMADO) AS MONTO_RECLAMADO, RECLAMO.CITA_ID
         FROM
         (
@@ -64,7 +63,7 @@ FROM
     ) C3 ON C3.CITA_ID = RECLAMO.CITA_ID
     INNER JOIN
     (
-        -- Subconsulta para obtener la suma de MONTO_DEBITADO
+        
         SELECT 'Total' AS NOMBRE, 'Total' AS SEXO, SUM(RECLAMO.MONTO_DEBITADO) AS MONTO_DEBITADO, RECLAMO.CITA_ID
         FROM
         (
@@ -96,8 +95,6 @@ FROM
     ) C4 ON C4.CITA_ID = RECLAMO.CITA_ID
     
     UNION ALL
-    
-    -- Subconsulta para agregar la fila de totales
     SELECT 'Total', 'Total', NULL, NULL, SUM(C3.MONTO_RECLAMADO), SUM(C4.MONTO_DEBITADO)
     FROM 
     (
